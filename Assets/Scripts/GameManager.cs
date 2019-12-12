18b1fdae8c92793f2ce366f441f2a2ebcfc3338c
protodroid-IB <40289918+protodroid-IB@users.noreply.github.com>
2019-12-12 11:38:14 +0800

    basic game loop implemented, scrolling background textures implemented, game difficulty settings implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public enum GameDifficulty
+{
+    VerySlow,
+    Slow,
+    Normal,
+    Fast,
+    VeryFast,
+    Stop
+}
+
+public class GameManager : MonoBehaviour
+{
+    #region  singleton
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (FindObjectsOfType<GameManager>().Length > 1)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    #endregion
+
+    // difficulty events and variables
+    public UnityEvent onStartGame, onDifficultyChange, onStopGame;
+
+    private GameDifficulty difficulty = GameDifficulty.VerySlow;
+    public GameDifficulty Difficulty
+    {
+        get => difficulty;
+        set
+        {
+            difficulty = value;
+            ChangeDifficulty();
+        }
+    }
+
+    // this controls the speed of all repeating objects in game
+    public float GameSpeed { get; private set; }
+
+
+
+    // calls the change difficulty event and changes the overall game speed
+    private void ChangeDifficulty()
+    {
+        switch (Difficulty)
+        {
+            case GameDifficulty.VerySlow:
+                GameSpeed = 0.2f;
+                break;
+
+            case GameDifficulty.Slow:
+                GameSpeed = 0.4f;
+                break;
+
+            case GameDifficulty.Normal:
+                GameSpeed = 0.6f;
+                break;
+
+            case GameDifficulty.Fast:
+                GameSpeed = 0.8f;
+                break;
+
+            case GameDifficulty.VeryFast:
+                GameSpeed = 1.0f;
+                break;
+
+            case GameDifficulty.Stop:
+                GameSpeed = 0.0f;
+                break;
+        }
+
+        onDifficultyChange?.Invoke();
+    }
+
+
+    // starts the game
+    [ContextMenu("Start Game")]
+    private void StartGame()
+    {
+        Difficulty = GameDifficulty.VerySlow;
+
+        onStartGame?.Invoke();
+    }
+
+    [ContextMenu("Stop Game")]
+    private void StopGame()
+    {
+        Difficulty = GameDifficulty.Stop;
+
+        onStopGame?.Invoke();
+    }
+
+    [ContextMenu("To Next Difficulty")]
+    public void ToNextDifficulty()
+    {
+        Difficulty = (GameDifficulty)(((int) Difficulty + 1) % (int)GameDifficulty.Stop);
+    }
+}
---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    private Material mat;
+
+    private float spriteWidth;
+
+    [SerializeField]
+    [Range(0.01f,10f)]
+    private float maxSpeed = 1f;
+    private float speed = 0f;
+
+    [SerializeField]
+    [Range(0.005f, 10f)]
+    private float acceleration = 1f;
+    private float speedLerpValue = 0;
+
+    private IDisposable speedChangeSub;
+    private IDisposable scrollingSub;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        mat = renderer.material;
+        spriteWidth = renderer.sprite.rect.width;
+
+        // if there is no game manager do not make subscriptions to events
+        if (!GameManager.instance)
+            return;
+
+        GameManager.instance.onStartGame.AddListener(StartScroll);
+        GameManager.instance.onDifficultyChange.AddListener(ChangeSpeed);
+        GameManager.instance.onStopGame.AddListener(StopScroll);
+    }
+
+
+    private void StartScroll()
+    {
+        ChangeSpeed();
+
+        scrollingSub = this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                float offset = mat.mainTextureOffset.x + Time.deltaTime * speed;
+
+                if (offset >= spriteWidth)
+                    offset = (offset - spriteWidth);
+
+                mat.mainTextureOffset = new Vector2(offset, 0f);
+            });
+    }
+
+
+    private void ChangeSpeed()
+    {
+        speedChangeSub?.Dispose();
+        speedLerpValue = 0;
+        float initialSpeed = speed;
+        float finalSpeed = maxSpeed * GameManager.instance.GameSpeed;
+
+        // lerp the speed and dispose of this subscription when speed has been reached
+        speedChangeSub = this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                speedLerpValue += Time.deltaTime * acceleration;
+                speedLerpValue = Mathf.Clamp01(speedLerpValue);
+                speed = Mathf.Lerp(initialSpeed, finalSpeed, speedLerpValue);
+
+                if (speedLerpValue >= 1)
+                    speedChangeSub.Dispose();
+            });
+    }
+
+
+    private void StopScroll()
+    {
+        speedChangeSub?.Dispose();
+        speedLerpValue = 0;
+
+        float initialSpeed = speed;
+        float finalSpeed = 0;
+
+        speedChangeSub = this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                speedLerpValue += Time.deltaTime * acceleration;
+                speedLerpValue = Mathf.Clamp01(speedLerpValue);
+                speed = Mathf.Lerp(initialSpeed, finalSpeed, speedLerpValue);
+
+                if (speedLerpValue >= 1)
+                    speedChangeSub.Dispose();
+            });
+
+    }
+}
